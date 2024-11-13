7507d085b139ec8160c2d856dec90e112eb01d89
akshay <akshayarao.parvam@gmail.com>
2024-11-13 10:43:28 +0530

    Day3 Backend - Model File

--- Day 2/backend/db.js
+++ Day 2/backend/db.js
@@ -66,7 +66,7 @@
 
 // Using Mongoose
 const mongoose = require('mongoose');
-const uri = 'mongodb://localhost:27017/trainer_db';
+const uri = 'mongodb://localhost:27017/trainer_akshay';
 const userSchema = new mongoose.Schema({
     name: String,
     age: Number
@@ -77,29 +77,29 @@ async function main() {
         await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB\n');
         //first User
-        const user = new User({ name: 'Alice', age: 25 });
+        const user = new User({ name: 'Alice', age: 25 });  //db.users.in
         await user.save();
         console.log('User saved:', user);
         const users = await User.find({});
         console.log('Found users:', users);
-        //second user
+        // //second user
         const newUser1 = new User({ name: 'John Doe', age: 30 });
         await newUser1.save();
         console.log('User created:', newUser1);
-        //third user
-        const newUser2 = new User({ name: 'Emily', age: 20 });
-        await newUser2.save();
-        console.log('User created:', newUser2);
-        //Read user by name
-        const user1 = await User.findOne({ _id: '6732a156b1e9119ee65dcf1c' });
+        // //third user
+        // const newUser2 = new User({ name: 'Emily', age: 20 });
+        // await newUser2.save();
+        // console.log('User created:', newUser2);
+        // //Read user by name
+        const user1 = await User.findOne({ _id: '673330cce5baa0fe354dd720' });
         console.log('User found:', user1);
-        //update a document
-        const updateUser = await User.updateOne({ _id: '6732a156b1e9119ee65dcf1c' }, { $set: { name: "Emily Martin" } });
+        // //update a document
+        const updateUser = await User.updateOne({ _id: '673330cce5baa0fe354dd720' }, { $set: { name: "Alice Martin" } });
         console.log('User updated:', updateUser);
         const users_updated = await User.find({});
         console.log('Found users:', users_updated);
-        //delete user 
-        const deleteUser = await User.deleteMany({ name: 'Emily' });
+        // //delete user 
+        const deleteUser = await User.deleteMany({ name: 'Alice' });
         const newUsers4 = await User.find({});
         console.log("users after deletions: ", newUsers4);
     }
